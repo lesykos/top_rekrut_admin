92e70a424e53f04373c57eb12a86817d938ce7f0
Lesyk <lesuk93@yahoo.com>
2026-05-01 21:44:44 +0300

    Update dataProvider to send Authorization header with token

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,5 +1,16 @@
+import { fetchUtils } from 'react-admin';
 import simpleRestProvider from "ra-data-simple-rest";
 
+const httpClient = (url: string, options: RequestInit = {}) => {
+    if (!options.headers) {
+        options.headers = new Headers({ Accept: 'application/json' });
+    }
+    const headers = options.headers as Headers;
+    const token = localStorage.getItem('token') || null;
+    headers.set('Authorization', `Bearer ${token}`);
+    return fetchUtils.fetchJson(url, options);
+};
+
 export const dataProvider = simpleRestProvider(
-  import.meta.env.VITE_SIMPLE_REST_URL,
+  import.meta.env.VITE_SIMPLE_REST_URL, httpClient
 );
